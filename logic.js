1a6de944ca97dfceede72d4bad13c384c6a17852
adam-taitano <79883367+adam-taitano@users.noreply.github.com>
2021-06-24 14:21:28 -0700

    Update logic.js
    start dock list implementation

--- logic.js
+++ logic.js
@@ -64,28 +64,85 @@ class DockList {
   constructor(list) {
     this.head = null;
     this.tail = null;
+    this.count = 0;
     if (list) {
-      list.forEach(dock => {this.append(dock)});
+      list.forEach(dock => {
+        this.append(dock);
+        ++this.count;
+      });
     }
   }
 
-  append() {
-
+  append(dock) {
+    if (!this.head && !this.tail) {
+      this.head = this.tail = dock;
+    }
+    else if (this.head === this.tail){
+      this.tail.next = dock;
+      dock.prev = this.tail;
+      this.tail = dock;
+    }
+    else {
+      this.tail.next = dock;
+      dock.prev = this.tail;
+      this.tail = dock;
+    }
+    ++this.count;
   }
 
   pop() {
-
+    if (!this.head && !this.tail) {
+      console.log("DockList is already empty");
+    }
+    else if (this.head === this.tail) {
+      this.head = this.tail = null;
+    }
+    else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
+    --this.count;
   }
 
-  traverse() {
+  findByLoc(locObj) {
+    //check if empty
+    if (this.count === 0) {
+      console.log("There are no docks to search.");
+      return null;
+    }
+    //parse x and y locs
+    let x = locObj.x;
+    let y = locObj.y;
+    //traverse list until match or at tail
+    current = this.head;
+    found = null;
+    while (current.next != null) {
+      //compare to each dock's x and y ranges
+      if (current.loc.xmin < x < current.loc.xmax && current.loc.ymin < y < current.loc.ymax) {
+        found = current;
+        break;
+      }
+      current = current.next;
+    }
+    return found;
+  }
 
+  findByName(head, name) {
+    if (!this.head) return null;
+    else if (head.name === name) {
+      return head;
+    }
+    else {
+      return findByName(head.next, name);
+    }
   }
 }
 
 //Port
 class Dock {
+
   //provide dock object with the following constructor properties:
-  constructor(name, zone, loc, dockTime, travelTime, next, prev, hasBoat) {
+  constructor(obj) {
     Object.assign(this, { name, zone, loc, dockTime, travelTime, next, prev, hasBoat });
     if (!this.next) { this.next = null }
     if (!this.prev) { this.prev = null }
